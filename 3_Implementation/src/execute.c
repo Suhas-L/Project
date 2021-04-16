c399c65973bbc561da627f6cbddee5ede0798580
Suhas-L <80455876+Suhas-L@users.noreply.github.com>
2021-04-16 15:27:18 +0530

    Update execute.c

--- 3_Implementation/src/execute.c
+++ 3_Implementation/src/execute.c
@@ -1,78 +1,85 @@
-#include "declfile.h"
+#include"declarations.h"
 
+// Function to execute the Examination
+// Management System
 void execute()
 {
-    printf(
-        " Enter the serial number"
-        "to select the option \n");
-    printf(" 1. To show Eligible"
-        "candidates \n");
-    printf(" 2. To delete the "
-        "student record \n");
-    printf(" 3. To change the "
-        "eligibility criteria \n");
-    printf(" 4. Reset the "
-        "eligibility criteria \n");
-    printf(" 5. Print the list "
-        "of all the student \n");
-    printf(" Enter 0 to exit \n");
+	printf("Enter the serial number "
+		"to select the option \n");
+	printf(" 1. To show Eligible"
+		"candidates \n");
+	printf(" 2. To delete the "
+		"student record \n");
+	printf(" 3. To change the "
+		"eligibility criteria \n");
+	printf(" 4. Reset the "
+		"eligibility criteria \n");
+	printf(" 5. Print the list "
+		"of all the student \n");
+	printf(" Enter 0 to exit \n");
+    int option;
+	scanf("%d", &option);
 
-    scanf("%d", &option);
+	// Switch Statement for choosing
+	// the desired option for user
+    float present = 75.00;
+    char money = 'P';
+	switch (option) {
+	case 1:
+		eligibleStudents(s);
+		execute();
+		break;
 
-    // Switch Statement for choosing
-    // the desired option for the user
-    switch (option) {
-    case 1:
-        eligibleStudents(s);
-        execute();
-        break;
+	case 2:
+		delete(s);
+		execute();
+		break;
 
-    case 2:
-        delete(s);
-        execute();
-        break;
+	case 3:
+		printf("Old Attendance "
+			"required = %f",
+			present);
+		printf(
+			"\n Enter the updated "
+			"attendence required \n");
+		scanf("%f", &present);
+		printf("fees status required"
+			" was %c \n",
+			money);
+		printf("Enter the new fees "
+			"status 'P' for paid 'N' "
+			"for not paid and "
+			"'B' for both \n");
+		scanf("%c", &money);
 
-    case 3:
-        printf("Old Attendance "
-            "required = %f",
-            present);
-        printf(
-            "\n Enter the updated "
-            "attendence required \n");
-        scanf("%f", &present);
-        printf("fees status required"
-            " was %c \n",
-            money);
-        printf("Enter the new fees "
-            "status 'P' for paid 'N' "
-            "for not paid and "
-            "'B' for both \n");
-        scanf("%c", &money);
-        printf("Eligibility Criteria updated \n");
-        execute();
-        break;
+		printf("Eligibility Criteria"
+			" updated \n");
+		execute();
+		break;
 
-    case 4:
-        present = 75.00;
-        money = 'P';
-        printf("Eligibility creitria reset \n");
-        execute();
-        break;
+	case 4:
+		present = 75.00;
+		money = 'P';
+		printf("Eligibility Criteria"
+			" reset \n");
+		execute();
+		break;
 
-    case 5:
-        print_students(s);
-        execute();
-        break;
+	case 5:
+		print_student(s);
+		execute();
+		break;
 
-    case 6:
-        execute();
-        break;
+	case 6:
+		execute();
+		break;
 
-    case 0:
-        exit(0);
+	case 0:
+		exit(0);
 
-    default:
-        printf("Enter number only from 0-4 \n");
-        execute();
-    }
+	default:
+		printf("Enter number only"
+			" from 0 - 4 \n");
+		execute();
+	}
 }
