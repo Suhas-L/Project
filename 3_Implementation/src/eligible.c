9dbce60f62af53125a90a6060509be659b16397f
Suhas-L <80455876+Suhas-L@users.noreply.github.com>
2021-04-16 15:26:45 +0530

    Update eligible.c

--- 3_Implementation/src/eligible.c
+++ 3_Implementation/src/eligible.c
@@ -1,29 +1,37 @@
-#include "declfile.h"
+#include"declarations.h"
 
+// Function to print the student
+// details of the eligible students
 void eligibleStudents(struct student s[])
-{
-    printf("________________"
-        "________________"
-        "_______________"
-        "_____________ \n");
-    printf("Qualified student are = \n");
+{   
+    float present = 75.00;
+    char money = 'P';
+    int n,i;
+	printf("________________"
+		"________________"
+		"_______________"
+		"_____________ \n");
+	printf("Qualified student are = \n");
 
-    // Iterate over the list
-    // of the students records
-    for (i = 0; i < n; i++) {
-        // Check for the eligibility
-        // of the student
-        if (s[i].fees == money || 'B' == money) {
-            if (s[i].attend >= present) {
-                printf("Student name = %s \n",
-                    s[i].name);
-                printf("Student roll no. = %d \n",
-                    s[i].rno);
-                printf(" Student fees = %c \n",
-                    s[i].fees);
-                printf(" Student attendence = %f \n",
-                    s[i].attend);
-            }
-        }
-    }
+	// Iterate over the list of the
+	// students records
+	for (i = 0; i < n; i++) {
+		// Condition to check the
+		// eligibility of the student
+		if (s[i].fees == money || 'B' == money) {
+			if (s[i].attend >= present) {
+				printf("Student name = %s \n",
+					s[i].name);
+
+				printf("Student roll no. = %d \n",
+					s[i].rno);
+
+				printf(" Student fees = %c \n",
+					s[i].fees);
+
+				printf(" Student attendence = %f \n",
+					s[i].attend);
+			}
+		}
+	}
 }
