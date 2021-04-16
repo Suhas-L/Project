5e876f6dd657ef2d23fa4e58388dea886d709773
Suhas-L <80455876+Suhas-L@users.noreply.github.com>
2021-04-16 15:16:33 +0530

    Update main.c

--- 3_Implementation/test/main.c
+++ 3_Implementation/test/main.c
@@ -1,41 +1,34 @@
-#include "declfile.h"
+// C program for the Examination
+// Management System
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include"declarations.h"
 
-
-
+// Driver Code
 int main()
 {
-    int option = 0;
-int i = 0;
-int n = 0;
-int j = 0;
-float present = 75.00;
-char money = 'P';
-float tdays = 1;
+	printf("Welcome to Student "
+		"database registration \n");
 
-    printf("Welcome to Student "
-        "database registration \n");
+	printf("Enter 0 to exit \n");
+	printf("Enter 1 to add student"
+		" record \n");
+    int option;
+	scanf("%d", &option);
 
-    printf("Enter 0 to exit \n");
-    printf("Enter 1 to add student"
-        " record \n");
+	// Switch Statements
+	switch (option) {
+	case 0:
+		exit(0);
 
-    scanf("%d", &option);
+	case 1:
+		add(s);
+		break;
 
-    // Switch Statements
-    switch (option) {
-    case 0:
-        exit(0);
-
-    case 1:
-        add(s);
-        break;
-
-    default:
-        printf("Only enter 0 or 1");
-        execute();
-    }
-    return 0;
+	default:
+		printf("Only enter 0 or 1");
+		execute();
+	}
+	return 0;
 }
