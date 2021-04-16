9d8471d599c5be8ed97f54a7825874ca2f849e3f
Suhas-L <80455876+Suhas-L@users.noreply.github.com>
2021-04-16 15:27:48 +0530

    Update print.c

--- 3_Implementation/src/print.c
+++ 3_Implementation/src/print.c
@@ -1,20 +1,21 @@
-#include "declfile.h"
-void printStudents(struct student s[])
-{
-    // Loop to iterate over the students
-    // students records
-    for (i = 0; i < n; i++) {
+#include"declarations.h"
 
-        printf("Name of student %s \n",
-            s[i].name);
-        printf("Student roll number = %d \n",
-            s[i].rno);
-        printf("Student fees status = %c \n",
-            s[i].fees);
-        printf("Student number of days "
-            "present = %f \n",
-            s[i].days);
-        printf("Student attendence = %f \n",
-            s[i].attend);
-    }
+// Function to print the students record
+void print_student(struct student s[])
+{   int n,i;
+	// Iterate over the students
+	// students records
+	for (i = 0; i < n; i++) {
+		printf("Name of student %s \n",
+			s[i].name);
+		printf("Student roll number = %d \n",
+			s[i].rno);
+		printf("Student fees status = %c \n",
+			s[i].fees);
+		printf("student number of days "
+			"present = %d \n",
+			s[i].days);
+		printf("Student attendence = %f \n",
+			s[i].attend);
+	}
 }
